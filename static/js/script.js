52b83d6c3222f527f1527656b252bb1aaf62edd2
lukehanson2429 <lukehanson2429@gmail.com>
2021-06-16 12:51:50 +0000

    datepicker custom jquery added

--- static/js/script.js
+++ static/js/script.js
@@ -9,4 +9,12 @@ $( document ).ready(function () {
     });
     $('select').formSelect();
     $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
