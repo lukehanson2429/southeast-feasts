9c8cdc96174caa6a46509b5d7e818dba8c6ee31c
lukehanson2429 <lukehanson2429@gmail.com>
2021-06-21 10:47:02 +0000

    delete functionality added

--- app.py
+++ app.py
@@ -184,6 +184,13 @@ def edit_recipe(recipe_id):
         "edit_recipe.html", recipe=recipe, countries=countries)
 
 
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    flash("Recipe Successfully Deleted")
+    return redirect(url_for('profile', username=session['user']))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
